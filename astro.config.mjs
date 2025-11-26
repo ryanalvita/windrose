9a47c3b5fc227656abd120609a6104c3be757e28
Alvita R. (Ryan) <Ryan.Alvita@vanoord.com>
2025-11-26 22:10:58 +0100

    Add github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://windrose.github.io',
+  base: '/windrose-astro/src/pages',
   integrations: [mdx(), sitemap()],
 
   vite: {
